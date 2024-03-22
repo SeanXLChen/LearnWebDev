b4e00ce6658ceb2eda51fc3bb6ea0ec114c1e498
SeanXLChen <xiaolaiseanchen@gmail.com>
2024-03-21 21:28:23 -0700

    fixed a issue

--- 7-react-3/src/App.jsx
+++ 7-react-3/src/App.jsx
@@ -11,7 +11,7 @@ export default function App() {
     <div className="app">
       <GeoForm setLatLng={setLatLng} />
 
-      {latLng && <WeatherChart latLng={latLng} />} /* conditional rendering */
+      {latLng && <WeatherChart latLng={latLng} />} 
     </div>
   );
 }
