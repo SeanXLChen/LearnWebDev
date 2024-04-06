cf7a7278440fe01a8ba41c5dba31351a9cc9d831
SeanXLChen <xiaolaiseanchen@gmail.com>
2024-04-05 21:47:22 -0700

    chapter 9 & 10 uploaded

--- 8-react-router-tutorial/seanapp/src/Nav.js
+++ 8-react-router-tutorial/seanapp/src/Nav.js
@@ -1,9 +1,24 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
-function Nav() {
+function Nav( {search, setSearch }) {
   return (
-    <nav>
-        <h1>Nav</h1>
+    <nav className="Nav">
+        <form className="searchForm" onSubmit={(e) => e.preventDefault()}>
+            <label htmlFor="search">Search Post</label>
+            <input 
+                type="search" 
+                id="search" 
+                placeholder='Search Posts'
+                value={search} 
+                onChange={(e) => setSearch(e.target.value)} 
+            />
+        </form>
+        <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/about">About</Link></li>
+            <li><Link to="/post">New Post</Link></li>
+        </ul>
     </nav>
   )
 }
